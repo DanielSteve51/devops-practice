6ad6fe9c9ad2784c3883ca1dd1ab4af047ca828f
Daniel Steve Gladson S <danielgladson51@gmail.com>
2026-03-27 12:10:48 +0530

    functinality change v2

--- src/main/java/com/Daniel/test/InputServlet.java
+++ src/main/java/com/Daniel/test/InputServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/divide")
+@WebServlet("/calculate")
 public class InputServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -21,9 +21,33 @@ public class InputServlet extends HttpServlet {
         try {
             int a = Integer.parseInt(request.getParameter("a"));
             int b = Integer.parseInt(request.getParameter("b"));
-            out.println("<h2>Result: " + (a / b) + "</h2>");
-        } catch (ArithmeticException e) {
-            out.println("<h3>Cannot divide by zero</h3>");
+            String op = request.getParameter("op");
+            int result;
+
+            if (op.equals("Add")) {
+                result = a + b;
+                out.println("<h2>Result: " + result + "</h2>");
+            } else if (op.equals("Subtract")) {
+                result = a - b;
+                out.println("<h2>Result: " + result + "</h2>");
+            } else if (op.equals("Multiply")) {
+                result = a * b;
+                out.println("<h2>Result: " + result + "</h2>");
+            } else if (op.equals("Divide")) {
+                if (b == 0) {
+                    out.println("<h3>Cannot divide by zero</h3>");
+                } else {
+                    out.println("<h2>Result: " + (double) a / b + "</h2>");
+                }
+            } else if (op.equals("Modulus")) {
+                if (b == 0) {
+                    out.println("<h3>Cannot mod by zero</h3>");
+                } else {
+                    result = a % b;
+                    out.println("<h2>Result: " + result + "</h2>");
+                }
+            }
+
         } catch (NumberFormatException e) {
             out.println("<h3>Not a number</h3>");
         } catch (Exception e) {
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,11 +1,16 @@
 <html>
 <body>
-<h2>Hello. This build is done using Jenkins -> Sonar QualityGate Check -> ECR -> ECS v1</h2>
+<h2>Hello. This build is done using Jenkins -> Sonar QualityGate Check -> ECR -> ECS v2</h2>
 
-<form action="<%= request.getContextPath() %>/divide" method="post">
+<form action="<%= request.getContextPath() %>/calculate" method="post">
     <input type="text" name="a" placeholder="Enter value1">
     <input type="text" name="b" placeholder="Enter value2">
-    <input type="submit" value="dividedValue">
+    <br><br>
+    <input type="submit" name="op" value="Add">
+    <input type="submit" name="op" value="Subtract">
+    <input type="submit" name="op" value="Multiply">
+    <input type="submit" name="op" value="Divide">
+    <input type="submit" name="op" value="Modulus">
 </form>
 
 </body>
